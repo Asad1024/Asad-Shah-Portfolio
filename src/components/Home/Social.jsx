7c50dfcfbb0d64abd14a9024b865773dd0be257a
shahgee77 <hashmigee96@gmail.com>
2023-01-18 02:40:24 +0500

    First commit

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/Asad1024"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/asad.ali.sh/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram-alt"></i>
+      </a>
+      <a
+        href="https://twitter.com/asad_ali_sh"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-twitter-alt"></i>
+      </a>
+    </div>
+  );
+};
